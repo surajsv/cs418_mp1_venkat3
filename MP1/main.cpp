69ee1f5743c6c3da86f710d03a7cf06297c26d91
Suraj Venkat <suraj.sv@gmail.com>
2016-02-09 01:43:06 -0600

    Initial Commit

---
+++ MP1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MP1
+//
+//  Created by Suraj Venkat on 2/9/16.
+//  Copyright © 2016 Suraj Venkat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
